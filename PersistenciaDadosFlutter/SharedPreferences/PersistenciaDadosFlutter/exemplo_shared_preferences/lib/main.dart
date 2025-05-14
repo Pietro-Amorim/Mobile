93c13d56535a250157d005b450965b185323cdd9
Kiwizin11 <pietro.fernandes@aluno.senai.br>
2025-05-14 14:45:19 -0300

    14.05.25

--- PersistenciaDadosFlutter/SharedPreferences/PersistenciaDadosFlutter/exemplo_shared_preferences/lib/main.dart
+++ PersistenciaDadosFlutter/SharedPreferences/PersistenciaDadosFlutter/exemplo_shared_preferences/lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:exemplo_shared_preferences/tela_inicial.dart';
 import 'package:flutter/material.dart';
-import 'tela_inical.dart';
+// import 'tela_inical.dart'; // Removed as the file does not exist
 
 void main(){
   runApp(MaterialApp(
