473ebb272fa2e7b7daf2dd169c05a5764a66b844
Pietro <pietro.fernandes@aluno.senai.br>
2025-08-06 15:57:05 -0300

    06.08.25

--- ApiWebServices/JsonConvert.dart
+++ ApiWebServices/JsonConvert.dart
@@ -1,14 +1,23 @@
-// Teste de Conversão de JSON <-> Dart
-import 'dart:convert'; // Biblioteca Nativa -> Não precisa baixar para o Pubspec
+//Teste de Conversão Json <-> Dart
+import 'dart:convert'; //nativa -> não precisa baixar para o pubspec
 
-void main() {
+void main(){
+  // tenho um texto em formato de Json
   String UsuarioJson = '''{
                             "id": "1ab2",
                             "user": "usuario1",
-                            "nome": "Pietro",
-                            "idade": 18,
-                            "cadastrado": true,
+                            "nome": "Pedro",
+                            "idade": 25,
+                            "cadastrado": true
                       }''';
-    Map<String, dynamic> UsuarioMap = jsonDecode(UsuarioJson);
-    print("ID: ${UsuarioMap['id']}");
+  // para manipular esse texto
+  //converter(decode) Json -> em Map
+  Map<String,dynamic> usuario = json.decode(UsuarioJson);
+  // Manipulando as informações do Json -> Map
+  print(usuario["idade"]);
+  usuario["idade"] = 26;
+  // converter(encode) Map -> Json
+  UsuarioJson = json.encode(usuario);
+  //tenho novamente um Json em formato de Texto
+  print(UsuarioJson); 
 }
